709cdd44a30fcfccc4db1a181f5b3f746b48980f
124119-tamura <tamura_ryosuke@comture.com>
2025-04-25 16:17:48 +0900

    amplify.yml

--- svelte-kit-to-amplify-app/svelte.config.js
+++ svelte-kit-to-amplify-app/svelte.config.js
@@ -1,9 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 export default {
   kit: {
     adapter: adapter({
-      fallback: 'index.html', // シングルページアプリケーション用
+      // Amplify に静的ホスティングさせるため fallback を指定
+      fallback: 'index.html'
     }),
+    paths: {
+      base: ''
+    }
   },
+  preprocess: vitePreprocess()
 };
